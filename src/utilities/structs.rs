5952e6625d72438a4131c03fa7c8371ff4b48e08
maxdesalle <max@sablier.finance>
2023-05-08 15:34:36 +0200

    feat: add hard link metadata integration

--- src/utilities/structs.rs
+++ src/utilities/structs.rs
@@ -1,6 +1,23 @@
 use std::fs::{Metadata, Permissions};
-use std::os::macos::fs::MetadataExt;
+use std::os::macos::fs::MetadataExt as OsMetadataExt;
+
 use std::time::SystemTime;
+
+// Function to be called if the target OS is MacOS
+#[cfg(target_os = "macos")]
+use std::os::unix::fs::MetadataExt;
+#[cfg(target_os = "macos")]
+fn number_of_links(metadata: &Metadata) -> u64 {
+    (metadata as &dyn MetadataExt).nlink()
+}
+
+// Function to be called if the target OS is Linux
+#[cfg(target_os = "linux")]
+use std::os::linux::fs::MetadataExt;
+#[cfg(target_os = "linux")]
+fn number_of_links(metadata: &Metadata) {
+    (metadata as &dyn MetadataExt).st_nlink()
+}
 
 pub struct Parameters {
     pub include_dot_files: bool,
@@ -24,7 +41,7 @@ impl Parameters {
 pub struct File {
     pub is_dir: bool,
     pub file_mode: Permissions,
-    pub number_of_links: u32,
+    pub number_of_links: u64,
     pub owner_name: String,
     pub group_name: String,
     pub number_of_bytes: u64,
@@ -37,7 +54,7 @@ impl File {
         File {
             is_dir: metadata.is_dir(),
             file_mode: metadata.permissions(),
-            number_of_links: 0,
+            number_of_links: number_of_links(&metadata),
             owner_name: metadata.st_gid().to_string(),
             group_name: metadata.st_gid().to_string(),
             number_of_bytes: metadata.len(),
